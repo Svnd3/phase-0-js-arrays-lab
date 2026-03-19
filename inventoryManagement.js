7602ced32df230549f65c3ee327bf6adb97276d7
Hezron Sande <hezronsande2@gmail.com>
2026-03-19 13:31:52 +0300

    added arraylist,neproduct function,and update feature

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,26 @@
 // Write your code here
+// Array of products
+let products = ['Laptop', 'Phone', 'Headphones', 'Monitor'];
 
+// Show the first product
+function logFirstProduct(){
+  console.log(products[0]);
+}
 
+//Add new product
+function addProduct(product){
+  products.push(product);
+
+}
+
+//update a product name
+function updateProductName(index, newName){
+  products[index] = newName;
+}
+//Remove the last product
+function removeLastProduct(){
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
